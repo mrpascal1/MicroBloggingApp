134085553944a38aa4cb46b7e70c385d95f8bcbb
MIDHAT <ansarimidhat45@gmail.com>
2023-06-21 19:57:30 +0530

    Added HomeFragment & divider

--- app/src/main/java/com/heuristic/microbloggingapp/HomeFragment.java
+++ app/src/main/java/com/heuristic/microbloggingapp/HomeFragment.java
@@ -26,7 +26,6 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
