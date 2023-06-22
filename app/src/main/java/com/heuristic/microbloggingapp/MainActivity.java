29566133e3f1f6e90b5d35cb76491a4ff96c228a
cphmp <becmicro@gmail.com>
2023-06-22 20:46:21 +0530

    Saad...Shah

--- app/src/main/java/com/heuristic/microbloggingapp/MainActivity.java
+++ app/src/main/java/com/heuristic/microbloggingapp/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+
     @Override
     protected void onResume() {
         super.onResume();
