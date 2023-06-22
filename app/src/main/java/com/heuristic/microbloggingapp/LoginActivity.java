c3effc7e1b29a7f3f7e073e5939e271cfcdaf6d4
SADAF SHAIKH <sadafnoor355@gmail.com>
2023-06-22 13:39:02 +0530

    ADDED LOGIN LOGIC

--- app/src/main/java/com/heuristic/microbloggingapp/LoginActivity.java
+++ app/src/main/java/com/heuristic/microbloggingapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.heuristic.microbloggingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,13 +11,31 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
  EditText username;
  EditText password;
  TextView nuser;
  MaterialButton logbtn;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
          password = findViewById(R.id.password);
          logbtn =   findViewById(R.id.logbtn);
          nuser = findViewById(R.id.nuser);
+         auth = FirebaseAuth.getInstance();
         logbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,14 +59,24 @@ public class LoginActivity extends AppCompatActivity {
                 if (TextUtils.isEmpty(pass)){
                     Toast.makeText(LoginActivity.this,"ENTER PASSWORD",Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-        nuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                startActivity(intent);}
-        });
+                auth.signInWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),"Login Successfull",Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
 
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+                    }
+                });
 }
 }
