c5cc2cc3965f9d47280c59013dee9aac3026fb83
SADAF SHAIKH <sadafnoor355@gmail.com>
2023-06-27 19:54:21 +0530

    REARRANGED LOGIN ACTIVITY CODE

--- app/src/main/java/com/heuristic/microbloggingapp/RegisterActivity.java
+++ app/src/main/java/com/heuristic/microbloggingapp/RegisterActivity.java
@@ -6,9 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,7 +14,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -42,36 +39,30 @@ public class RegisterActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Users");
 
-      binding.SignUpbtn.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            String musername = binding.username.getText().toString().trim();
-            String memail = binding.email.getText().toString().trim();
-            String mpassword = binding.password.getText().toString().trim();
+      binding.SignUpbtn.setOnClickListener(v -> {
+        String musername = binding.username.getText().toString().trim();
+        String memail = binding.email.getText().toString().trim();
+        String mpassword = binding.password.getText().toString().trim();
 
-            if(!musername.equals("") && !memail.equals("") && mpassword.length()>6)
-            {
+        if(!musername.equals("") && !memail.equals("") && mpassword.length()>6)
+        {
 
-                //Login
-                fetchUserFromEmail(musername, memail, mpassword);
+            //Login
+            fetchUserFromEmail(musername, memail, mpassword);
 
-            }
+        }
 
-            else
-            {
-                Toast.makeText(RegisterActivity.this,"Please enter proper values", Toast.LENGTH_SHORT).show();
-            }
-          }
+        else
+        {
+            Toast.makeText(RegisterActivity.this,"Please enter proper values", Toast.LENGTH_SHORT).show();
+        }
       });
 
 
-      binding.existingUser.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-              startActivity(intent);
-              finish();
-          }
+      binding.existingUser.setOnClickListener(v -> {
+          Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+          startActivity(intent);
+          finish();
       });
 
 
@@ -79,42 +70,33 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void fetchUserFromEmail(String username, String email, String password) {
         firebaseAuth.fetchSignInMethodsForEmail(email)
-                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
-                        if (task.isSuccessful()) {
-                            List<String> users = task.getResult().getSignInMethods();
-                            if (users != null)
-                            if (!users.isEmpty()) {
-                                Toast.makeText(RegisterActivity.this, "User Already exist", Toast.LENGTH_SHORT).show();
-                            } else {
-                                registerNewUser(username, email, password);
-                            }
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<String> users = task.getResult().getSignInMethods();
+                        if (users != null)
+                        if (!users.isEmpty()) {
+                            Toast.makeText(RegisterActivity.this, "User Already exist", Toast.LENGTH_SHORT).show();
+                        } else {
+                            registerNewUser(username, email, password);
                         }
                     }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
+                }).addOnFailureListener(e -> {
 
-                    }
                 });
     }
 
 
     private void registerNewUser(String username, String email, String password) {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
 
-                            String userId = task.getResult().getUser().getUid();
-                            User user = new User(userId, username, email);
-                            addUserToDB(user);
-                            //Insert to db
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "Register failed", Toast.LENGTH_SHORT).show();
-                        }
+                        String userId = task.getResult().getUser().getUid();
+                        User user = new User(userId, username, email);
+                        addUserToDB(user);
+                        //Insert to db
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Register failed", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -134,11 +116,6 @@ public class RegisterActivity extends AppCompatActivity {
                         startActivity(intent);
                         finish();
                     }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(RegisterActivity.this, "Database failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                }).addOnFailureListener(e -> Toast.makeText(RegisterActivity.this, "Database failed", Toast.LENGTH_SHORT).show());
     }
 }
