f5973e04d25a0e120fbee2f4d00c6ce0a99b7eb7
afsha <afshaikh210426@gmail.com>
2023-07-15 02:55:00 +0530

    USER PROFILE DESIGN

---
+++ app/src/main/java/com/heuristic/microbloggingapp/userprofile.java
@@ -0,0 +1,12 @@
+package com.heuristic.microbloggingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class userprofile extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
